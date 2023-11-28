dc7e9662fc7e17d6f978b6f9682430aaae9b5e24
shimarrudz <vic.shima.vss@gmail.com>
2023-11-28 01:09:39 -0300

    fix: add more style

--- PsyCheck/src/screens/Quiz/style.tsx
+++ PsyCheck/src/screens/Quiz/style.tsx
@@ -2,36 +2,41 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  padding: 20px;
-  justify-content: center;
-  align-items: center;
+  padding: 16px;
   background-color: #f5f5f5;
 `;
 
 export const Title = styled.Text`
   font-size: 24px;
   font-weight: bold;
-  margin-bottom: 30px;
+  color: black;
+  margin-top: 16px;
+  margin-left: 120px;
 `;
 
 export const QuestionText = styled.Text`
   font-size: 20px;
-  margin-bottom: 10px;
+  margin-top: 35px;
+  color: black;
 `;
 
 export const AnswerInput = styled.TextInput`
   width: 100%;
-  border: 1px solid #ddd;
+  height: 50px;
   padding: 10px;
-  margin-bottom: 20px;
+  margin-top: 5px;
+  font-size: 16px;
+  border-radius: 5px;
+  background-color: #ededed;
 `;
 
 export const Button = styled.TouchableOpacity`
-  width: 88%;
+  width: 100%;
   height: 56px;
   background-color: #FF85D0;
   border-radius: 12px;
   justify-content: center;
+  margin-top: 35px;
   align-items: center;
 `;
 
