95b7fe3ffbc4df5cdbb733340943d960e409a2f9
Thiago Castro <thiagogyorgy.castro@gmail.com>
2023-11-28 23:20:29 -0300

    alterando e começando página de diagnóstico

---
+++ PsyCheck/src/components/DiagnosisResultCards/index.tsx
@@ -0,0 +1,50 @@
+export const diagnosisResultData = [
+  {
+    id: '1',
+    name: 'Casas Bahia',
+    sales: 120,
+    image: require('../../assets/images/diagnosis-result-casas-bahia.png'),
+  },
+  {
+    id: '2',
+    name: 'KaBuM!',
+    sales: 98,
+    image: require('../../assets/images/diagnosis-result-KaBuM!.png'),
+  },
+  {
+    id: '3',
+    name: 'Kalunga',
+    sales: 95,
+    image: require('../../assets/images/diagnosis-result-kalunga.png'),
+  },
+  {
+    id: '4',
+    name: 'Mercado Livre',
+    sales: 78,
+    image: require('../../assets/images/diagnosis-result-mercado-libre.png'),
+  },
+  {
+    id: '5',
+    name: 'TeraByte',
+    sales: 64,
+    image: require('../../assets/images/diagnosis-result-terabyte.png'),
+  },
+  {
+    id: '6',
+    name: 'Shopee',
+    sales: 38,
+    image: require('../../assets/images/diagnosis-result-shopee.png'),
+  },
+  {
+    id: '7',
+    name: 'Google',
+    sales: 15,
+    image: require('../../assets/images/diagnosis-result-google.png'),
+  },
+  {
+    id: '8',
+    name: 'Amazon',
+    sales: 8,
+    image: require('../../assets/images/diagnosis-result-amazon.png'),
+  },
+];
