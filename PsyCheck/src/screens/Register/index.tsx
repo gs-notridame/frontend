9fbe0d7cb5b529e4ce2c3ffcb61a851cf62177f7
shimarrudz <vic.shima.vss@gmail.com>
2023-12-01 03:04:41 -0300

    fix: correct register form

--- PsyCheck/src/screens/Register/index.tsx
+++ PsyCheck/src/screens/Register/index.tsx
@@ -1,16 +1,10 @@
 import React, {useState} from 'react';
 import RegisterForm from '../../components/RegisterForm';
-import {
-  Container,
-  ContentContainer,
-  BackgroundImage,
-  Logo,
-  Button,
-  ButtonText,
-} from './style';
+import {Container, ContentContainer, BackgroundImage, Logo} from './style';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {RootStackParamList} from '../../types/rock-stack-param-list';
 import Toast from 'react-native-toast-message';
+import {RegisterButton} from '../../components/RegisterButton';
 
 type RegisterScreenNavigationProp = StackNavigationProp<
   RootStackParamList,
@@ -22,20 +16,18 @@ type Props = {
 };
 
 const Register: React.FC<Props> = ({navigation}) => {
+  const [name, setName] = useState<string>('');
+  const [numSeguroSaude, setNumSeguroSaude] = useState<string>('');
   const [email, setEmail] = useState<string>('');
+  const [cpf, setCpf] = useState<string>('');
   const [password, setPassword] = useState<string>('');
   const [confirmPassword, setConfirmPassword] = useState<string>('');
-  const [cnpj, setCnpj] = useState<string>('');
-  const [loading, setLoading] = useState<boolean>(false);
-
   const isPasswordValid = (password: string) => {
-    // Verifica se a senha tem pelo menos 6 caracteres e contém um caractere maiúsculo
     return password.length >= 6 && /[A-Z]/.test(password);
   };
 
-  const handleRegister = () => {
-    // Validações
-    if (!email || !password || !confirmPassword || !cnpj) {
+  const handleRegister = async () => {
+    if (!email || !password || !confirmPassword || !cpf) {
       Toast.show({
         type: 'error',
         text1: 'Erro',
@@ -53,7 +45,7 @@ const Register: React.FC<Props> = ({navigation}) => {
       return;
     }
 
-    if (cnpj.length !== 14) {
+    if (cpf.length !== 11) {
       Toast.show({
         type: 'error',
         text1: 'Erro',
@@ -74,6 +66,49 @@ const Register: React.FC<Props> = ({navigation}) => {
       });
       return;
     }
+    try {
+      const registerData = {
+        historico: 'Histórico do paciente',
+        numSeguroSaude: numSeguroSaude,
+        pessoa: {
+          nome: name,
+          email: email,
+          usuario: 'usuário_paciente',
+          cpf: cpf,
+          senha: password,
+          softDelete: false,
+        },
+      };
+      const response = await fetch('http://172.19.0.1:8080/paciente', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(registerData),
+      });
+
+      if (response.ok) {
+        Toast.show({
+          type: 'success',
+          text1: 'Sucesso',
+          text2: 'Cadastro realizado com sucesso!',
+        });
+        navigation.navigate('Login');
+      } else {
+        Toast.show({
+          type: 'error',
+          text1: 'Erro',
+          text2: 'Não foi possível realizar o cadastro.',
+        });
+      }
+    } catch (error) {
+      console.error(error);
+      Toast.show({
+        type: 'error',
+        text1: 'Erro de Conexão',
+        text2: 'Não foi possível conectar ao servidor.',
+      });
+    }
   };
   return (
     <Container>
@@ -82,22 +117,21 @@ const Register: React.FC<Props> = ({navigation}) => {
         <ContentContainer>
           <Logo source={require('../../assets/images/logo-psycheck.png')} />
           <RegisterForm
+            name={name}
+            setName={setName}
+            numSeguroSaude={numSeguroSaude}
+            setNumSeguroSaude={setNumSeguroSaude}
             email={email}
             setEmail={setEmail}
+            cpf={cpf}
+            setCpf={setCpf}
             password={password}
             setPassword={setPassword}
             confirmPassword={confirmPassword}
             setConfirmPassword={setConfirmPassword}
-            cnpj={cnpj}
-            setCnpj={setCnpj}
             onRegisterPress={handleRegister}
           />
-          <Button
-            onPress={() => {
-              navigation.navigate('Login');
-            }}>
-            <ButtonText>Cadastro</ButtonText>
-          </Button>
+          <RegisterButton onPress={handleRegister} text="Cadastrar" />
         </ContentContainer>
       </BackgroundImage>
     </Container>
